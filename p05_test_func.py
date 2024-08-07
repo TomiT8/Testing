4761b84f8fb13f0adfcec5f16a0cf002d6f39d4e
TomiT8 <tomas.jasko8@gmail.com>
2024-08-07 20:39:49 +0200

    uprava

--- p05_test_func.py
+++ p05_test_func.py
@@ -1,6 +1,7 @@
 def func(string):
     str_string = str(string)[::2]
-    str_string = int(str_string) *2
+    str_string = int(str_string) * 2
     return int(str_string)
+
 
 print(func(12883))  # 183*2 = 366
