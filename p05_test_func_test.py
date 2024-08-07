367513da07e81d570c10cb44abad21f36ebf5771
TomiT8 <tomas.jasko8@gmail.com>
2024-08-07 20:51:22 +0200

    pridaný assert na precvičenie

--- p05_test_func_test.py
+++ p05_test_func_test.py
@@ -5,3 +5,15 @@ from .p05_test_func import *
 @pytest.mark.parametrize("number, result", [(12, 2), (11, 2), (2, 4), (123, 26), (92, 18), (79, 14), (19023, 206)])
 def test_func(number, result):
     assert func(number) == result
+
+# todo
+#   doplnené pre precvičenie
+def test_func2():
+    assert func(12) == 2
+    assert func(11) == 2
+    assert func(2) == 4
+    assert func(123) == 26
+    assert func(92) == 18
+    assert func(79) == 14
+    assert func(19023) == 206
+    
