45c80456860cc28d13fbcf9c629403a7a7de2044
TomiT8 <tomas.jasko8@gmail.com>
2024-08-12 18:31:32 +0200

    upravený p09_Exercise

--- p09_Exercise_word_count.py
+++ p09_Exercise_word_count.py
@@ -1,8 +1,21 @@
+# todo
+#   ideálny stav, ak je vždy za slovom biely znak
+# def word_count(word):
+#     if len(word) == 0:
+#         return 0
+#     else:
+#         return word.count(" ") + 1
+#
+#
+# print(word_count(""))
+
+import re
+
+
 def word_count(word):
+    word = word.strip()
     if len(word) == 0:
         return 0
-    else:
-        return word.count(" ") + 1
+    words = re.findall(r'\b\w+\b', word)
 
-
-print(word_count(""))
+    return len(words)
