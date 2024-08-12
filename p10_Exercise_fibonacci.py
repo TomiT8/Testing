4f3c12e5f0fcc4927981347db990c4e506a4f68f
TomiT8 <tomas.jasko8@gmail.com>
2024-08-12 19:15:01 +0200

    pridany p10_Exercise_fibonacci

--- p10_Exercise_fibonacci.py
+++ p10_Exercise_fibonacci.py
@@ -7,4 +7,6 @@ def fibonacci(num):
         return fibonacci(num - 1) + fibonacci(num - 2)
 
 
-print(fibonacci(10))
+print(fibonacci(10))    # 55
+print(fibonacci(1))     # 1
+print(fibonacci(5))     # 5
